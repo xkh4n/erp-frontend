2a2419438d42b746a3251e8f5ebac59c4391c16f
bitrojas <bitbucket.org.01@gmail.com>
2024-12-10 17:47:00 -0300

    Creation init proyect

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes enviar los datos a tu servidor o realizar otra lógica
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LoginForm;
