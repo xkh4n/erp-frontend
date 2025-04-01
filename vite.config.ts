90edf7ef22f24e9ab8e691c0204dec005066ad0d
xkh4n <osvaldorojasg@gmail.com>
2025-04-01 18:57:42 -0300

    Update ^roject to Vite & Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css:{
+    postcss:{
+      plugins:[tailwindcss, autoprefixer]
+    }
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
 })
