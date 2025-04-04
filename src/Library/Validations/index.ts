f27fb22e924b0cc1da0e7fd5bc8819bbd4c3a30e
xkh4n <osvaldorojasg@gmail.com>
2025-04-04 18:50:09 -0300

    Formulario de Login

---
+++ src/Library/Validations/index.ts
@@ -0,0 +1,104 @@
+const IsUsername = (username: string) => {
+    // Verificar si el valor no es una cadena
+    if (typeof username !== 'string') {
+        console.warn("The input is not a string");
+        return false;
+    }
+    // Eliminar espacios en blanco al inicio y al final
+    username = username.trim();
+    const regex = /^[a-z]{1,20}$/;
+    // Verificar si el valor no cumple con la expresión regular
+    if (!regex.test(username)) {
+      console.warn(`The input "${username}" is not a valid username`);
+      return false;
+    }
+    return true;
+};
+
+const IsPassword = (password:string) => {
+    // Expresión regular para validar contraseñas
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[.,;:!?'%$&#?¡@"()\[\]{}\-_*]).{8,}$/;
+    // Verificar si el valor no es una cadena o no cumple con la expresión regular
+    if (typeof password !== 'string' || !regex.test(password)) {
+      console.warn("The input is not a valid password");
+      return false;
+    }
+    return true;
+};
+
+
+const IsIata = (iata:string) => {
+    const regex = /^[a-zA-ZÁÉÍÓÚáéíóúñÑ0-9]{3}$/;
+    if (typeof iata !== 'string' || !regex.test(iata)) {
+      console.warn("The input is not a valid IATA code");
+      return false;
+    }
+    return true;
+};
+
+const IsEmail = (email:string) => {
+    // Expresión regular para validar un correo electrónico
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    // Verificar si el valor no es una cadena o no cumple con la expresión regular
+    if (typeof email !== 'string' || !regex.test(email)) {
+      console.warn("The input is not a valid E-Mail");
+      return false;
+    }
+    return true;
+};
+
+const IsParagraph = (paragraph:string) => {
+    // Verificar si el valor no es una cadena
+    if (typeof paragraph !== 'string') {
+      console.warn("The input is not a string");
+      return false;
+    }
+    // Eliminar espacios en blanco al inicio y al final (opcional, pero recomendado)
+    paragraph = paragraph.trim();
+    // Expresión regular para validar párrafos
+    const regex = /^[a-zA-Z0-9\s.,;:!?'"()\[\]{}\-_*\/&@#%^~|\\+=]{1,}$/;  
+    // Verificar si el valor no cumple con la expresión regular
+    if (!regex.test(paragraph)) {
+      console.warn(`The input "${paragraph}" is not a valid paragraph`);
+      return false;
+    }
+    return true;
+};
+
+const IsDecimal = (decimal:number) => {
+    // Expresión regular para validar números decimales
+    const regex = /^-?\d+(\,\d+)?$/;
+    // Verificar si el valor no es una cadena o no cumple con la expresión regular
+    if (typeof decimal !== 'string' || !regex.test(decimal)) {
+        console.warn("The input is not a valid decimal number");
+        return false;
+    }
+    return true;
+}
+
+const IsPhone = (phone:string) => {
+    // Verificar si el valor no es una cadena
+    if (typeof phone !== 'string') {
+        console.warn("The input is not a string");
+        return false;
+    }
+    // Eliminar espacios en blanco al inicio y al final (opcional, pero recomendado)
+    phone = phone.trim();
+    // Expresión regular para validar números de teléfono
+    const regex = /^(?:\+56)? ?(?:9 ?)?\d{4}(?: ?\d{4})?$/;
+    // Verificar si el valor no cumple con la expresión regular
+    if (!regex.test(phone)) {
+        console.warn(`The input "${phone}" is not a valid phone number`);
+        return false;
+    }
+    return true;
+}
+export{
+    IsUsername,
+    IsPassword,
+    IsIata,
+    IsEmail,
+    IsParagraph,
+    IsDecimal,
+    IsPhone,
+}
--- src/Views/Login/index.tsx
+++ src/Views/Login/index.tsx
@@ -1,11 +1,54 @@
 import { Link } from 'react-router-dom'
 import { logInOutline } from 'ionicons/icons'
 import { IonIcon } from '@ionic/react'
+import { IsEmail, IsPassword} from '../../Library/Validations'
+import { useState } from 'react'
+import {TextBox} from '../../Components/Input'
+import axios from 'axios'
+
+
 import './Login.css'
+
+
 export default function index() {
-    const handlerSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(false)
+    //Función para validar el formulario y enviar los datos al servidor
+    const handlerSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        alert('login')
+        const formData = new FormData(e.currentTarget)
+        const email = formData.get('email')?.toString() || ''
+        setEmailError(!IsEmail(email))
+        const password = formData.get('password')?.toString() || ''
+        setPasswordError(!IsPassword(password))
+        try {
+            alert(!emailError && !passwordError);
+            if(!emailError && !passwordError){
+                const response = await axios.post(`${import.meta.env.VITE_API_URL}/login`, {
+                    email,
+                    password
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    timeout: 3000 // timeout de 5 segundos
+                })
+                alert(`${import.meta.env.VITE_API_URL}/login`)
+                console.log(response.status)
+                // Manejar login exitoso
+            }
+            
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                // Manejar errores específicos de la API
+                console.error('Error de login:', error.response?.data)
+            } else {
+                // Manejar errores inesperados
+                console.error('Error inesperado:', error)
+            }
+        }
     }
     return (
         <div className="bg-image w-full sm:w-1/2 md:w-9/12 lg:w-1/2 mx-3 md:mx-5 lg:mx-0 flex flex-col md:flex-row items-center rounded-lg z-10 overflow-hidden bg-center bg-cover bg-blue-600 shadow-xl shadow-blue-800/50">
@@ -19,14 +62,32 @@ export default function index() {
             </div>
             <div className="w-full md:w-1/2 flex flex-col items-center bg-white py-5 md:py-8 px-4 justify-around">
                 <h3 className="mb-4 font-bold text-3xl flex items-center text-blue-500">Inicio de Sesión</h3>
-                <form className='' onSubmit={handlerSubmit}>
-                    <input 
-                        type="email" placeholder="email.."
-                        className=" mb-3 px-4 py-2 w-full rounded border border-gray-300 shadow-sm text-base placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:border-blue-500"
+                <form className='w-full' onSubmit={handlerSubmit}>
+                    <TextBox
+                        name="email"
+                        type="email"
+                        placeholder="email.."
+                        label="Email"
+                        value={email}
+                        error={emailError}
+                        errorMessage="Por favor, ingrese un email válido"
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                            setEmailError(false)
+                        }}
                     />
-                    <input 
-                        type="password" placeholder="password.."
-                        className="mb-3 px-4 py-2 w-full rounded border border-gray-300 shadow-sm text-base placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:border-blue-500"
+                    <TextBox
+                        name="password"
+                        type="password"
+                        placeholder="password.."
+                        label="Password"
+                        value={password}
+                        error={passwordError}
+                        errorMessage="La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un carácter especial"
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                            setPasswordError(false)
+                        }}
                     />
                     <button type='submit' className="flex justify-center items-center bg-blue-950 hover:bg-red-600 shadow-red-600/50 text-white focus:outline-none focus:ring py-2 w-full rounded-full shadow-xl hover:shadow-blue-800/50 transition delay-10 duration-300 ease-in-out hover:translate-y-1">
                         <IonIcon icon={logInOutline} className="w-5 h-5" />
